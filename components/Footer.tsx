6cb9afc33ea14875a1b79754149f3ee1bf8d6f23
Nagwa Houmid El Amrani <psynh3@exmail.nottingham.ac.uk>
2025-11-22 18:05:31 +0000

    added footer

---
+++ components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="w-full py-6 text-center text-xs text-neutral-500">
+      © 2025 Nagwa Houmid. All rights reserved.
+    </footer>
+  );
+}
+
